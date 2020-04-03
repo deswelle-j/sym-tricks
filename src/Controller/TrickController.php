0ded2d21efefd8929cfab305d0e900e81aedb2c5
deswelle-j <deswelle.johan@gmail.com>
2020-04-03 11:23:48 +0200

    #8-create a basic form and a function to add new trick

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Trick;
+use App\Form\TrickType;
+use Doctrine\ORM\EntityManager;
 use App\Repository\TrickRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TrickController extends AbstractController
@@ -18,7 +23,34 @@ class TrickController extends AbstractController
         ]);
     }
 
-        /**
+    /**
+     * @Route("/trick/new", name="trick_new")
+     * 
+     * @return Response
+     */
+
+    public function new(Request $request)
+    {
+        $trick = new Trick();
+
+
+        $form = $this->createForm(TrickType::class, $trick);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($trick);
+
+            $manager->flush();
+        }
+
+        return $this->render('trick/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
      * @Route("/trick/{id}", name="trick_show")
      * 
      * @return Response
@@ -26,8 +58,9 @@ class TrickController extends AbstractController
     public function show($id, TrickRepository $repo)
     {
         $trick =$repo->findOneById($id);
-        return $this->render('trick/index.html.twig', [
+        return $this->render('trick/show.html.twig', [
             'trick' => $trick,
         ]);
     }
+
 }
