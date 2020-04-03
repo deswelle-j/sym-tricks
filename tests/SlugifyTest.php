96706eb27704bd93cca3a5f9361f103a4a7606e8
deswelle-j <deswelle.johan@gmail.com>
2020-04-03 17:14:47 +0200

    #8-create a service to convert title into a slug

---
+++ tests/SlugifyTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\App\Service;
+
+use App\Service\Slugify;
+use PHPUnit\Framework\TestCase;
+
+class SlugifyTest Extends TestCase
+{
+    /**
+     * @dataProvider titleToSlugify
+     */
+    public function testSlugifyTitle($title, $expectedSlug)
+    {
+        $slugify = new Slugify();
+        $slug = $slugify->slugify($title);
+
+        $this->assertSame($expectedSlug, $slug);
+    }
+
+    public function titleToSlugify()
+    {
+        return [
+            ["Backside Air", "backside-air"],
+            [" Switch Backside Rodeo 720 ", "switch-backside-rodeo-720"],
+            ["BS 540 Seatbelt", "bs-540-seatbelt"]
+        ];
+    }
+}
