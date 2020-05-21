46d54104bd2996e77c49286d198ade07885066ee
deswelle-j <deswelle.johan@gmail.com>
2020-05-21 19:25:32 +0200

    #17 - install security bundle and create a security.yaml file, add uniqueEntity on user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,9 +3,14 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(
+ *  fields={"username"},
+ *  message="Cet utilisateur existe déjà"
+ * )
  */
 class User
 {
