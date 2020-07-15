f632b576f3d40ea275507bf5c835c2cbacff3122
deswelle-j <deswelle.johan@gmail.com>
2020-07-15 20:14:50 +0200

    #18 - create unit test for UserVerify

---
+++ tests/UserVerifyTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\App\Service;
+
+use App\Entity\User;
+use App\Service\UserVerify;
+use Doctrine\ORM\EntityManager;
+use PHPUnit\Framework\TestCase;
+
+class UserVerifyTest Extends TestCase
+{
+
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $entityManager;
+
+    protected function setUp(): void
+    {
+
+        $this->entityManager = $this->getMockBuilder(EntityManager::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    /**
+     * @dataProvider tokenToVerify
+     */
+    public function testTokenVerify($username, $Usertoken, $token)
+    {
+
+        $user = new User();
+        $user->setUsername($username);
+        $user->setToken($Usertoken);
+
+        $userVerify = new UserVerify($this->entityManager);
+        $result = $userVerify->tokenVerify($username, $user, $token);
+
+        $this->assertTrue($result);
+    }
+
+    public function tokenToVerify()
+    {
+        return [
+            ["john", "tokengenerick1204684235", "tokengenerick1204684235"]
+        ];
+    }
+
+}
