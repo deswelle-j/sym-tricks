e72689e0bb7e1989d2e44182f7b9cbd7145b7ad3
deswelle-j <deswelle.johan@gmail.com>
2020-07-31 15:52:56 +0200

    create unit test for loginFormAuthenticator

---
+++ tests/LoginFormAuthenticatorTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\tests;
+
+use Doctrine\ORM\EntityManager;
+use PHPUnit\Framework\TestCase;
+use Doctrine\Persistence\ObjectManager;
+use App\Security\LoginFormAuthenticator;
+use Doctrine\Persistence\ObjectRepository;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+
+class LoginFormAuthenticatorTest extends TestCase
+{
+
+    public function testExceptionThrowWhenTokenIsInvalid()
+    {
+        $csrfTokenManager = $this->getMockBuilder(CsrfTokenManagerInterface::class)
+            ->getMockForAbstractClass();
+        $csrfTokenManager->method('isTokenValid')->willReturn(false);
+        $urlGenerator = $this->getMockBuilder(UrlGeneratorInterface::class)
+            ->getMockForAbstractClass();
+        $entityManager = $this->getMockBuilder(EntityManager::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $passwordEncoder = $this->getMockBuilder(UserPasswordEncoderInterface::class)
+        ->getMockForAbstractClass();
+        
+        $loginFormAuth = new LoginFormAuthenticator($entityManager,  $urlGenerator, $csrfTokenManager,  $passwordEncoder);
+        $credentials = [
+            'username' => 'usernametest',
+            'password' => '1234',
+            'csrf_token' => '_csrf_token',
+        ];
+
+        $userProvider = $this->getMockBuilder(UserProviderInterface::class)
+            ->getMockForAbstractClass();
+
+        // $this->expectException(InvalidCsrfTokenException::class);
+
+        $loginFormAuth->getUser($credentials, $userProvider);
+
+    }
+
+    public function testExceptionThrowWhenUsernameCouldNotBeFound()
+    {
+        $csrfTokenManager = $this->getMockBuilder(CsrfTokenManagerInterface::class)
+            ->getMockForAbstractClass();
+
+        $csrfTokenManager->method('isTokenValid')->willReturn(true);
+
+        $urlGenerator = $this->getMockBuilder(UrlGeneratorInterface::class)
+            ->getMockForAbstractClass();
+
+        $entityManager = $this->getMockBuilder(EntityManager::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $passwordEncoder = $this->getMockBuilder(UserPasswordEncoderInterface::class)
+            ->getMockForAbstractClass();
+        $loginFormAuth = new LoginFormAuthenticator($entityManager,  $urlGenerator, $csrfTokenManager,  $passwordEncoder);
+        $credentials = [
+            'username' => 'usernametest',
+            'password' => '1234',
+            'csrf_token' => '_csrf_token',
+        ];
+
+        $user = new User();
+        $user->setHash($credentials['password']);
+        $user->setToken($credentials['csrf_token']);
+
+        $userProvider = $this->getMockBuilder(UserProviderInterface::class)
+            ->getMockForAbstractClass();
+
+        $userRepository = $this->createMock(ObjectRepository::class);
+        $userRepository->expects($this->any())
+            ->method('find')
+            ->willReturn($user);
+
+        $objectManager = $this->createMock(ObjectManager::class);
+        
+        $objectManager->expects($this->any())
+            ->method('getRepository')
+            ->willReturn($userRepository);
+
+        $loginFormAuth->getUser($credentials, $userProvider);
+    
+    }
+
+}
