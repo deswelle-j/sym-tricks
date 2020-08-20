d0a1843da34625c32ea7f4da9c40c5c2b1b9dc1c
deswelle-j <deswelle.johan@gmail.com>
2020-08-20 18:04:20 +0200

    fix use for commentEntity

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -3,10 +3,11 @@
 namespace App\Entity;
 
 use DateTime;
+use App\Entity\Comment;
 use App\Service\Slugify;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
