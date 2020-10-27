8763fb67ce10dbb6245db115327099bf4ae3c974
deswelle-j <deswelle.johan@gmail.com>
2020-10-27 14:41:54 +0100

    install bootsrap with webpack encore

--- assets/app.js
+++ assets/app.js
@@ -9,7 +9,10 @@
 import './styles/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-import $ from 'jquery';
-global.$ = global.jQuery = $;
+const $ = require('jquery');
+require('bootstrap');
 
+// $(document).ready(function() {
+//     $('[data-toggle="popover"]').popover();
+// });
 console.log('Hello Webpack Encore! Edit me in assets/app.js');
