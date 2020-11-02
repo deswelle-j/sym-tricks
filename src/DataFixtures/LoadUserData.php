7c2a5259f68ba64ce4e76ac86e40d023128261ee
deswelle-j <deswelle.johan@gmail.com>
2020-11-02 17:14:19 +0100

    #21 update fixture

---
+++ src/DataFixtures/LoadUserData.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadUserData extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $user = new User;
+        $user->setUsername('flaty')
+             ->setEmail('flaty@example.com')
+             ->setHash('$2y$13$MbAljDu6zvR73tInFYZ2m.kBJHEmskyPTk/L0ZKKsN5LM9ThSCFlS')
+             ->setActive(true)
+             ->setAvatarPath('thumb-212196.jpg');
+
+        $manager->persist($user);
+
+        $this->addReference('user-flaty', $user);
+
+        $manager->flush();
+
+    }
+
+    public function getOrder()
+    {
+        return 3; // ordre d'appel
+    }
+}
