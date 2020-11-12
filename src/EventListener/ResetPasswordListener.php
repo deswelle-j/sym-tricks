860d81f7b2f4633b990b4ec5188ffd3b7fff9041
deswelle-j <deswelle.johan@gmail.com>
2020-11-12 15:58:40 +0100

    #21 - create template for password forgotten email

--- src/EventListener/ResetPasswordListener.php
+++ src/EventListener/ResetPasswordListener.php
@@ -4,6 +4,7 @@ namespace App\EventListener;
 
 use App\Event\ResetPasswordEvent;
 use Symfony\Component\Mime\Email;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
@@ -27,13 +28,18 @@ class ResetPasswordListener
         $token = $user->getToken();
         $userId = $user->getId();
         $url = $this->router->generate('user_reset_password', ['userId' => $userId, 'token' => $token], UrlGeneratorInterface::ABSOLUTE_URL);
-        $email = (new Email())
+        $email = (new TemplatedEmail())
             ->from($this->send_email)
             ->to($user->getEmail())
             ->priority(Email::PRIORITY_HIGH)
             ->subject('Réinitialisation de mot de passe sur Snowtrick')
             ->text("{$url}")
-            ->html('<p>See Twig integration for better HTML integration!</p>');
+            ->context([
+                'user' => $user,
+                'token' => $token
+            ])
+            ->text("{$url}")
+            ->htmlTemplate('email/passwordForgotten.html.twig');
 
         $this->mailer->send($email);
     }
